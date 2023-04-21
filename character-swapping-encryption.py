2b2c459bbeb680d96dd4d43c0a783376a0e4354c
Mary Irish P. Cammagay <maryirish.cammagay@gmailcom>
2023-04-21 17:22:56 +0800

    import pyfiglet and create title

--- character-swapping-encryption.py
+++ character-swapping-encryption.py
@@ -1,5 +1,11 @@
 # import module
+import pyfiglet
+
 # title
+title = "ALL-TIME DECRYPTION TOOL"
+font = pyfiglet.figlet_format(title, font="standard", width = 250)
+print(font.center(100))
+
 # make function
 # ask user for encrypted texts
 # decrypt the user input
