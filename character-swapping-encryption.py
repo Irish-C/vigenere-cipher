db8c6a7acc10b4fe37703de8e666261109012f00
Mary Irish P. Cammagay <maryirish.cammagay@gmailcom>
2023-04-21 17:27:25 +0800

    add function for decrypting encrypted characters

--- character-swapping-encryption.py
+++ character-swapping-encryption.py
@@ -6,7 +6,17 @@ title = "ALL-TIME DECRYPTION TOOL"
 font = pyfiglet.figlet_format(title, font="standard", width = 250)
 print(font.center(100))
 
-# make function
+# function for decrypting text
+def decrypted_str(encrypted_str):
+    vowels_dict = {'*':'a', '&':"e", '#':'i', '+':'o', '!':'u'}
+    decrypted_str = ""
+    for i in encrypted_str:
+        if i in vowels_dict:
+            decrypted_str += vowels_dict[i]
+        else:
+            decrypted_str += i
+    return decrypted_str
+
 # ask user for encrypted texts
 # decrypt the user input
 # print result
