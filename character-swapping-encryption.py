3630f782d101bd60d203d154872e172f0876e606
Mary Irish P. Cammagay <maryirish.cammagay@gmailcom>
2023-04-21 16:47:00 +0800

    initial commit

---
+++ character-swapping-encryption.py
@@ -0,0 +1,9 @@
+# 1: pseudocode
+# ask user for encrypted text
+# check each character 
+#   if "*", change to "a"
+#   if "&", change to "e"
+#   if "#", change to "i"
+#   if "+", change to "o"
+#   if "!", change to "u"
+# print output
